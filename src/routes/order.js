120709bc0f2cd106ffbddc847fb7ea2a59b22ca8
Maria S <mariasantiago0109@gmail.com>
2023-05-05 14:48:30 -0400

    changed getAllOrders route

--- src/routes/order.js
+++ src/routes/order.js
@@ -8,7 +8,8 @@ const  {
 } = require('../controllers/order')
 
 router.route('/').post(createOrder)
-router.route('/:id').get(getAllOrders).get(getOrder).delete(deleteOrder)
+router.route('/user/:id').get(getAllOrders)
+router.route('/:id').get(getOrder).delete(deleteOrder)
 
 
 module.exports = router;
